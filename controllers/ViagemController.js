0feca44baa6b4c25c3cc30105dfba2d380faeb9b
Douglas Vidal <vidaldl01@gmail.com>
2022-10-25 19:56:32 -0300

    Update douglas
    aoishdioahs

--- controllers/ViagemController.js
+++ controllers/ViagemController.js
@@ -1,11 +1,4 @@
 const Viagem = require('../models/Viagem')
-const Cliente = require('../models/Cliente')
-const Usuario = require('../models/Usuario')
-const Veiculo = require('../models/Veiculo')
-const Viagem_Veiculos = require('../models/Viagem_Veiculos')
-
-const sequelize = require('sequelize')
-const session = require('express-session')
 
 const { Op } = require('sequelize')
 
@@ -20,24 +13,6 @@ module.exports = class ViagemController {
         let ordenar = 'DESC'
 
         Viagem.findAll({
-
-            attributes: {
-                include: [
-                    "id",
-                    [sequelize.fn("DATE_FORMAT", sequelize.col("dataRetorno"), "%d/%m/%Y"),
-                        "dataRetorno"],
-                    [sequelize.fn("DATE_FORMAT", sequelize.col("dataPartida"), "%d/%m/%Y"),
-                    "dataPartida"],
-                    "origem",
-                    "destino",
-                    "valorTotal",
-                    "ClienteId",
-                ],
-            },
-            include: [{
-                model: Cliente,
-                required: true
-            }],
 
             order: [['createdAt', ordenar]], // ordernar pela coluna 'createdAt'
             limit: 1000,                     // limite de 1000 resultados
@@ -60,23 +35,8 @@ module.exports = class ViagemController {
     // --------------------------------------------------------------------------
     // FUNÇÃO PARA REDIRECIONAR PARA A PÁGINA DE CRIAR VIAGEM
     // --------------------------------------------------------------------------
-    static async criarViagem(req, res) {
-
-        let ordenar = 'ASC'
-
-        Veiculo.findAll({
-            order: [['tipo', ordenar]],
-            limit: 10,
-        })
-        .then((data) => {
-
-            const veiculos = data.map((result) => result.get({ plain: true }))
-            console.log(veiculos)
-            res.render('viagem/criar', { veiculos })
-
-        })
-        .catch((err) => console.log(err))
-
+    static criarViagem(req, res) {
+        res.render('viagem/criar')
     }
 
 
@@ -91,10 +51,9 @@ module.exports = class ViagemController {
             destino: req.body.destino,
             dataPartida: req.body.dataPartida,
             dataRetorno: req.body.dataRetorno,
-            veiculo: req.body.valorTotal,
             usuarioCriacao: "john"
         }
-        
+        console.log(viagem)
         Viagem.create(viagem)
             .then(() => {
                 res.redirect('/viagem/')
--- models/Cliente.js
+++ models/Cliente.js
@@ -4,28 +4,8 @@ const bancoDeDados = require("../banco_de_dados/conexaoBD")
 
 const Cliente = bancoDeDados.define("Cliente", {
 
-    nome:{
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    nomeFantasia:{
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-
-    cpfCnpj:{
-        type: DataTypes.STRING(14),
-        allowNull: false
-    },
-
-    tipoPessoa:{
-        type: DataTypes.CHAR,
-        allowNull: false
-    },
-
-    usuarioCriacao:{
-        type: DataTypes.STRING(20),
+    teste:{
+        type: DataTypes.DECIMAL(10,2),
         allowNull: false
     }
 
