af7e405de0e169c5ee76134c05a3e82638cc9ef7
John Marcos <47645918+johnmarcosz@users.noreply.github.com>
2022-10-25 21:30:15 -0300

    Update Cliente.js

--- models/Cliente.js
+++ models/Cliente.js
@@ -4,11 +4,35 @@ const bancoDeDados = require("../banco_de_dados/conexaoBD")
 
 const Cliente = bancoDeDados.define("Cliente", {
 
-    teste:{
-        type: DataTypes.DECIMAL(10,2),
+    tipo:{
+        type: DataTypes.CHAR,
+        allowNull: false
+    },
+    
+    nomeRazaoSocial:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    
+    nomeFantasia:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    
+    cpfCnpj:{
+        type: DataTypes.STRING(14),
+        allowNull: false
+    },
+    
+    telefone:{
+        type: DataTypes.STRING(11), 
+        allowNull: false
+    },
+
+    usuarioCriacao:{
+        type: DataTypes.STRING(20),
         allowNull: false
     }
-
 })
 
 module.exports = Cliente
