a626dde5af41488341862c41a5c6d6e37292ff31
John Marcos <47645918+johnmarcosz@users.noreply.github.com>
2022-12-04 20:56:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Comandos para iniciar o projeto e instalar os pacotes
 // npm init -y
-// npm install express express-handlebars sequelize mysql2 bcryptjs connect-flash cookie-parser cookie-session express-flash express-session session-file-store nodemon node-windows
+// npm install express express-handlebars sequelize mysql2 bcryptjs connect-flash cookie-parser cookie-session express-flash express-session session-file-store nodemon
 
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
@@ -13,9 +13,15 @@ const aplicacao = express();
 
 const conexaoBD = require("./banco_de_dados/conexaoBD");
 
-aplicacao.engine("handlebars", expressHandlebars.engine());
+aplicacao.engine("handlebars", expressHandlebars.engine({
+  defaultLayout: 'main',
+  runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+  },
+}));
 aplicacao.set("view engine", "handlebars");
-
+ 
 aplicacao.use(
   express.urlencoded({
     extended: true,
@@ -28,24 +34,24 @@ aplicacao.use(express.static('public'))
 //middleware do controle de sessão
 aplicacao.use(
   session({
-    name: 'session',
-    secret: 'nosso_secret',
-    resave: false,
-    saveUninitialized: false,
-    store: new FileStore({
-      logFn: function () { },
-      path: require('path').join(require('os').tmpdir(), 'sessions'),
-    }),
-    cookie: {
-      secure: false,
-      maxAge: 3600000,
-      expires: new Date(Date.now() + 3600000),
-      httpOnly: true,
-    },
+  name: 'session',
+  secret: 'nosso_secret',
+  resave: false,
+  saveUninitialized: false,
+  store: new FileStore({
+  logFn: function () {},
+  path: require('path').join(require('os').tmpdir(), 'sessions'),
   }),
-)
-// flash messages
-aplicacao.use(flash());
+  cookie: {
+  secure: false,
+  maxAge: 3600000,
+  expires: new Date(Date.now() + 3600000),
+  httpOnly: true,
+  },
+  }),
+  )
+  // flash messages
+  aplicacao.use(flash());
 
 // Importa os Models para a criação das tabelas
 const Usuario = require("./models/Usuario");
@@ -53,7 +59,7 @@ const Motorista = require("./models/Motorista");
 //const Pessoa = require("./models/Pessoa");
 const Viagem = require("./models/Viagem");
 const Veiculo = require("./models/Veiculo");
-//const Viagem_Veiculos = require("./models/Viagem_Veiculos");
+const Viagem_Motoristas = require("./models/Viagem_Motoristas");
 const Orcamento = require("./models/Orcamento");
 const Cliente = require("./models/Cliente");
 
@@ -61,22 +67,22 @@ const Cliente = require("./models/Cliente");
 const verificaSessao = require("./models/sessao").verificaSessao;
 //Rota inicial da aplicação
 
-aplicacao.get('/', verificaSessao, function (req, res) {
-  res.render('home')
+aplicacao.get('/',verificaSessao, function (req, res) {
+res.render('home')
 })
 aplicacao.get('/login', function (req, res) {
-  res.render('login', { layout: false })
-})
+  res.render('login',{layout:false})
+  })
 
 //Logout
 aplicacao.get('/logout', function (req, res) {
   req.session.destroy()
   res.redirect('/login')
-})
+  })
 
 
 // ACESSE localhost:3000/usuarioTemp PARA CRIAR UM USUÁRIO PADRÃO TEMPORARIAMENTE
-aplicacao.get('/usuarioTemp', function (req, res) {
+aplicacao.get('/usuarioTemp', function(req, res){
 
   const Usuario = require('./models/Usuario')
   const bcrypt = require('bcryptjs')
@@ -86,18 +92,18 @@ aplicacao.get('/usuarioTemp', function (req, res) {
   const hashSenha = bcrypt.hashSync("admin", salt)
 
   const usuario = {
-    //Cria o hash
-    username: 'admin',
-    senha: hashSenha, //Usa o hash para cadastrar no bd
-    nome: 'Admin temporário'
+      //Cria o hash
+      username: 'admin',
+      senha: hashSenha, //Usa o hash para cadastrar no bd
+      nome: 'Admin temporário'
   }
 
   console.log(usuario)
   Usuario.create(usuario)
-    .then(() => {
-      res.redirect('/')
-    })
-    .catch((err) => console.log(err))
+      .then(() => {
+          res.redirect('/')
+      })
+      .catch((err) => console.log(err))
 })
 
 
@@ -116,11 +122,11 @@ aplicacao.use("/motorista", motoristaRotas);
 const clienteRotas = require("./routes/clienteRotas");
 aplicacao.use("/cliente", clienteRotas);
 
-const orcamentoRotas = require("./routes/orcamentoRotas");
-aplicacao.use("/orcamento", orcamentoRotas);
-
 const usuarioRotas = require("./routes/usuarioRotas.js");
 aplicacao.use("/usuario", usuarioRotas);
+
+const orcamentoRotas = require("./routes/orcamentoRotas");
+aplicacao.use("/orcamento", orcamentoRotas);
 
 
 // Inicia o servidor/aplicação somente depois de conectar ao BD
@@ -128,6 +134,6 @@ conexaoBD
   .sync()
   .then(() => {
     console.log("Conectado ao banco!")
-    aplicacao.listen(3001);
+    aplicacao.listen(3000);
   })
   .catch((err) => console.log(err));
