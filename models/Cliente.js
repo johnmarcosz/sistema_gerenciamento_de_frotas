bc7d21c10c5813c5eaf411ac5615448c60871316
John Marcos <47645918+johnmarcosz@users.noreply.github.com>
2022-12-04 20:58:26 -0300

    Update Cliente.js

--- models/Cliente.js
+++ models/Cliente.js
@@ -22,16 +22,18 @@ const Cliente = bancoDeDados.define("Cliente", {
     cpfCnpj:{
         type: DataTypes.STRING(14),
         allowNull: false
-        
     },
+    
     telefone:{
         type: DataTypes.STRING(12), 
         allowNull: false
     },
+    
     email: {
         type: DataTypes.STRING,
         allowNull: true,
     },
+
     usuarioCriacao:{
         type: DataTypes.STRING(20),
         allowNull: false
